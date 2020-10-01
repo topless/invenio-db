161bd142a57dd70a4e359c76ef01e02c08c38752
Chris Topaloudis <topless@gmail.com>
2020-10-01 10:27:23 +0200

    test fixes

--- run-tests.sh
+++ run-tests.sh
@@ -6,9 +6,9 @@
 # Invenio is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
 
+docker-services-cli up ${DB} && \
 python -m check_manifest --ignore ".travis-*" && \
 python -m sphinx.cmd.build -qnNW docs docs/_build/html && \
-docker-services-cli up ${DB}
 python -m pytest
 tests_exit_code=$?
 docker-services-cli down
